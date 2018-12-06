0ae13ec0f5d6556070699618469734e81f329cde
whirlwind <whirlwind110@163.com>
2018-12-06 22:04:01 +0800

    clean pulllog-box

--- fuckcsdn.js
+++ fuckcsdn.js
@@ -1,9 +1,5 @@
 document.getElementById('article_content').style=null;
 var ad =document.getElementsByClassName('hide-article-box text-center')[0];
 ad.parentNode.removeChild(ad);
-
-var ad =document.getElementsByClassName('adblock')[0];
-ad.parentNode.removeChild(ad);
-
-var ad =document.getElementsByClassName('check-adblock-bg')[0];
+var ad =document.getElementsByClassName('pulllog-box')[0];
 ad.parentNode.removeChild(ad);
